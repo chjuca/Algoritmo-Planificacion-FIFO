2a838278d67900cbbd989631835fdc020550305a
chjuca <chjuca@utpl.edu.ec>
2020-05-04 17:44:59 -0500

    Agregando Calculo de los TE y TR

--- src/app/components/process/process.component.ts
+++ src/app/components/process/process.component.ts
@@ -9,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class ProcessComponent implements OnInit {
 
   process = {} as Process;
-  processList = [];
+  // processList = [ {name: 'P1', cpuBurst: 3 , arrivalTime: 3, standbyTime : 0, returnTime: 0},
+  //                 {name: 'P2', cpuBurst: 4 , arrivalTime: 1},
+  //                 {name: 'P3', cpuBurst: 2 , arrivalTime: 5}
+  //               ];
+
+  processList = [ {name: 'P1', cpuBurst: 3 , arrivalTime: 2, standbyTime : 0, returnTime: 0},
+                  {name: 'P2', cpuBurst: 1 , arrivalTime: 4},
+                  {name: 'P3', cpuBurst: 3 , arrivalTime: 0},
+                  {name: 'P4', cpuBurst: 4 , arrivalTime: 1},
+                  {name: 'P5', cpuBurst: 2 , arrivalTime: 3}
+];
+  // processList = [];
+  is_clicked = false;
 
 
   constructor() { }
@@ -17,9 +29,36 @@ export class ProcessComponent implements OnInit {
   ngOnInit() {
   }
 
-  addProcess(procces: Process) {
-    this.processList.push({name: procces.name, cpuBurst: procces.cpuBurst, arrivalTime: procces.arrivalTime});
+  // Metodo para agregar un proceso al arreglo
+  addProcess(process: Process) {
+    this.processList.push({name: process.name, cpuBurst: process.cpuBurst, arrivalTime: process.arrivalTime,
+      standbyTime: process.standbyTime, returnTime :  process.returnTime});
+  }
+
+  // Metodo para ordenar la Lista por tiempo de llegada asc*
+  sortListbyArivalTime() {
+    this.processList.sort((a, b) => a.arrivalTime - b.arrivalTime);
+  }
+  sortListbyName() {
+    this.processList.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  // Metodo para aplicar el algoritmo FIFO al array de Objetos
+  fifo() {
+    this.sortListbyArivalTime();
+    this.processList[0].standbyTime = 0;
+    for ( let i = 0; i < this.processList.length; i++) {
+      this.processList[i].standbyTime = 0;
+      for ( let j = i - 1; j >= 0; j--) {
+        this.processList[i].standbyTime += this.processList[j].cpuBurst;
+      }
+      this.processList[i].returnTime = this.processList[i].standbyTime + this.processList[i].cpuBurst + this.processList[0].arrivalTime;
+      this.processList[i].standbyTime -= this.processList[i].arrivalTime;
+      this.processList[i].standbyTime += this.processList[0].arrivalTime;
+    }
     console.log(this.processList);
+    this.sortListbyName()
+    this.is_clicked = true;
   }
 
 
--- src/app/interfaces/process.interface.ts
+++ src/app/interfaces/process.interface.ts
@@ -1,5 +1,7 @@
 export interface Process {
     name: string;
-    cpuBurst: number  // posible cambio de nombre
-    arrivalTime: number // posible cambio de nombre tmbn
+    cpuBurst: number; // posible cambio de nombre
+    arrivalTime: number; // posible cambio de nombre tmbn
+    standbyTime?: number;  // posible cambio de nombre tmbn
+    returnTime?: number; // posible cambio de nombre tmbn
 }
