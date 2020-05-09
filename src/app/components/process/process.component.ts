c2af4d6e7cad3245ff0ea55616341a1945efeaff
chjuca <chjuca@utpl.edu.ec>
2020-05-09 11:08:43 -0500

    Agregue comentarios para mejor entendimiento del codigo

--- src/app/components/process/process.component.ts
+++ src/app/components/process/process.component.ts
@@ -9,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class ProcessComponent implements OnInit {
 
   process = {} as Process;
+
+  // lista de Procesos para probar algoritmo con tiempo muerto no es necesario, sirve para probar codigo mas rapido
+
   // processList = [ {name: 'P1', cpuBurst: 3 , arrivalTime: 3, standbyTime : 0, returnTime: 0},
   //                 {name: 'P2', cpuBurst: 4 , arrivalTime: 1},
   //                 {name: 'P3', cpuBurst: 2 , arrivalTime: 5}
   //               ];
+
+  // lista de Procesos para probar algoritmo sin tiempo muerto no es necesario, sirve para probar codigo mas rapido
 
   processList = [ {name: 'P1', cpuBurst: 3 , arrivalTime: 2, standbyTime : 0, returnTime: 0},
                   {name: 'P2', cpuBurst: 1 , arrivalTime: 4},
@@ -21,6 +26,8 @@ export class ProcessComponent implements OnInit {
                   {name: 'P5', cpuBurst: 2 , arrivalTime: 3}
 ];
   // processList = [];
+
+  // Bandera para mostrar la tabla de resultados;
   is_clicked = false;
 
 
@@ -29,7 +36,7 @@ export class ProcessComponent implements OnInit {
   ngOnInit() {
   }
 
-  // Metodo para agregar un proceso al arreglo
+  // Metodo para agregar un proceso a la lista
   addProcess(process: Process) {
     this.processList.push({name: process.name, cpuBurst: process.cpuBurst, arrivalTime: process.arrivalTime,
       standbyTime: process.standbyTime, returnTime :  process.returnTime});
@@ -39,26 +46,33 @@ export class ProcessComponent implements OnInit {
   sortListbyArivalTime() {
     this.processList.sort((a, b) => a.arrivalTime - b.arrivalTime);
   }
+
+
+  // Metodo para ordenar la Lista por nombre asc*
   sortListbyName() {
     this.processList.sort((a, b) => a.name.localeCompare(b.name));
   }
 
-  // Metodo para aplicar el algoritmo FIFO al array de Objetos
+  // Metodo para aplicar el algoritmo FIFO a la lista de Objetos
+
   fifo() {
-    this.sortListbyArivalTime();
-    this.processList[0].standbyTime = 0;
+    this.sortListbyArivalTime();                  // Ordenamos la lista por orden de llegada
+    this.processList[0].standbyTime = 0;          // Asignamos al primer proceso que llego espera 0
     for ( let i = 0; i < this.processList.length; i++) {
-      this.processList[i].standbyTime = 0;
+      this.processList[i].standbyTime = 0;        // Asignamos al proceso valor inicial o
       for ( let j = i - 1; j >= 0; j--) {
-        this.processList[i].standbyTime += this.processList[j].cpuBurst;
+        this.processList[i].standbyTime += this.processList[j].cpuBurst; // iteramos hasta calcular el valor de espera
       }
+
+      // Agregamos el valor de la rafaga del primer proceso en llegar, en el caso que no llegue en el tiempo 0
+
       this.processList[i].returnTime = this.processList[i].standbyTime + this.processList[i].cpuBurst + this.processList[0].arrivalTime;
       this.processList[i].standbyTime -= this.processList[i].arrivalTime;
       this.processList[i].standbyTime += this.processList[0].arrivalTime;
     }
-    console.log(this.processList);
-    this.sortListbyName()
-    this.is_clicked = true;
+    console.log(this.processList);  // presentamos la lista y los valores calculados
+    this.sortListbyName();          // Ordenamos la lista por orden nombre
+    this.is_clicked = true;         // Cambiamos de valor a la bandera para que presente la tabla de resultados
   }
 
 
