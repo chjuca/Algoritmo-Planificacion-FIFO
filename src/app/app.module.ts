595951d431534bfbb0402f0e7146d9f96858cfdf
renatojobal <renatojobal@gmail.com>
2020-05-11 09:04:21 -0500

    Fixed dir error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProcessComponent } from './components/process/process.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
