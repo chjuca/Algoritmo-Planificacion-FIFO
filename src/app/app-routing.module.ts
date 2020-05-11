30b4d1bdd60de86d825864b61fc8360af913091d
renatojobal <renatojobal@gmail.com>
2020-05-11 10:09:10 -0500

    Added navbar url (root)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { ProcessComponent } from './components/process/process.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
-  {path: "process", component: ProcessComponent}
+  {path: "process", component: ProcessComponent},
+  {path: "", component: NavbarComponent}
 ];
 
 @NgModule({
