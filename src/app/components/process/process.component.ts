d71f442879ebe6218c2da4d4a320fa1ab5b34228
chjuca <chjuca@utpl.edu.ec>
2020-05-13 14:34:31 -0500

    Agregando Grafica de Gantt

--- src/app/components/process/process.component.ts
+++ src/app/components/process/process.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProcessComponent implements OnInit {
 
   process = {} as Process;
+  process0 = {} as Process;
   avgTE: string;
   avgTR: string;
   processList = [];
@@ -32,6 +33,7 @@ export class ProcessComponent implements OnInit {
   // Metodo para aplicar el algoritmo FIFO a la lista de Objetos
   fifo() {
     this.processService.fifo();
+    this.process0 = this.processList[0];
     this.avgTE = this.processService.avgTE().toFixed(2);
     this.avgTR = this.processService.avgTR().toFixed(2);
     this.is_clicked = true;         // Cambiamos de valor a la bandera para que presente la tabla de resultados
